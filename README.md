a84c741c7e7cfba7e5daccce91e8d2a1dc150174
Jack Garbus <jackgarbus@gmail.com>
2019-01-25 18:17:18 -0500

    woot

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Tarski Counter
 
 ### Number calculated from world_counter.py:
-1.7699584814226504e+29
-
+176995848142265024237468868617
 $$
 \sum_{n=0}^{12}3^n2^n\binom{64}{n}(n+1)^6
 $$
--- world_counter.py
+++ world_counter.py
@@ -5,7 +5,7 @@ def ncr(n, r):
     r = min(r, n-r)
     numer = reduce(op.mul, range(n, n-r, -1), 1)
     denom = reduce(op.mul, range(1, r+1), 1)
-    return numer / denom
+    return int(numer / denom)
 
 f = open("chart.txt")
 world_count = 0;
