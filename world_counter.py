b5223ac3eb6bdb06dc2fbcf9035dcf3f89fc2173
Jack Garbus <jackgarbus@gmail.com>
2019-04-01 19:38:06 -0400

    edited world counter

--- world_counter.py
+++ world_counter.py
@@ -12,8 +12,7 @@ world_count = 0;
 data = f.readlines()
 for n in range(13):
     data[n] = data[n].split()
-    data[n].reverse()
     for l in range(n+1):
         for j in range(65):
-            world_count += int(data[l][j])*(3**n)*(2**(n-l))*((n+1)**6)*ncr(j,n-l)
+            world_count += int(data[l][j])*(3**n)*(2**(n-l))*((n+1)**6)*ncr(64-j,n-l)
 print("There are",world_count,"tarski worlds. This number has",len(str(world_count)),"digits!")
